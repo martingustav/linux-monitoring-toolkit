0c27c66ec68feedfe9f34089f44229d8bc838de4
Martin <martin@malon.se>
2026-06-11 12:22:13 +0200

    Add system health report script w. initial printout

---
+++ monitor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -e "=== System Health Report ===\n"
+
+# Output machine's hostname
+echo "Hostname: $(hostname)"
+
+# Output date and time
+echo "Date: $(date +%F\ %T)"
+
+# Output system usage
+# get_cpu_usage()
+# get_memory_usage()
+# get_disk_usage()
+# get_uptime()
+# get_top_memory_process()
